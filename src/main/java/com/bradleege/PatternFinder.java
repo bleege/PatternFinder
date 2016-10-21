684f16081778d7e53100e173c79cda3dfe2209c6
Brad Leege <bleege@gmail.com>
2016-10-21 00:14:17 -0500

    Adding pattern mapping and length check

--- src/main/java/com/bradleege/PatternFinder.java
+++ src/main/java/com/bradleege/PatternFinder.java
@@ -1,5 +1,7 @@
 package com.bradleege;
 
+import java.util.HashMap;
+
 public class PatternFinder {
 
     public PatternFinder() {
@@ -7,6 +9,22 @@ public class PatternFinder {
     }
 
     public boolean doesWordMatchPattern(String pattern, String word) {
+
+        HashMap<Integer, Object> patternChars = new HashMap<Integer, Object>();
+        int lengthOfPattern = 0;
+        for (int lc = 0; lc < pattern.length(); lc++) {
+            char c = pattern.charAt(lc);
+            patternChars.put(lc, c);
+            if (c >= '0' && c <= '9') {
+                lengthOfPattern = lengthOfPattern + Character.getNumericValue(c);
+            } else {
+                lengthOfPattern = lengthOfPattern + 1;
+            }
+        }
+
+        if (lengthOfPattern != word.length()) {
+            return false;
+        }
 
         return true;
     }
